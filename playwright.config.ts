94fd6aa98a93c42d903b10c56f69e3c680106f33
shark-x <charming020793@gmail.com>
2023-12-21 16:40:57 +0500

    set eslint rules and added 2 eslint scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,22 +8,22 @@ import path from 'path';
  */
 const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 switch (environment){
-  case 'development':
-    dotenv.config({path:path.resolve(__dirname,'.','.env')});
-    break
-  case 'production':
-    dotenv.config({path:path.resolve(__dirname,'.','.env.prod')});
-    break
-  default:
-    throw new Error('You use incorrect NODE_ENV environment. Available to use: development, production');
+case 'development':
+   dotenv.config({path:path.resolve(__dirname,'.','.env')});
+   break
+case 'production':
+   dotenv.config({path:path.resolve(__dirname,'.','.env.prod')});
+   break
+default:
+   throw new Error('You use incorrect NODE_ENV environment. Available to use: development, production');
 }
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL ? process.env.BASE_URL : "https://demoqa.com/",
-  },
+   use: {
+      /* Base URL to use in actions like `await page.goto('/')`. */
+      baseURL: process.env.BASE_URL ? process.env.BASE_URL : 'https://demoqa.com/',
+   },
 });
