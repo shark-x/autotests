fca1052e3c5803df9de6ab2ed5354e5f390bedaf
shark-x <charming020793@gmail.com>
2023-12-26 00:30:40 +0500

    edited auth test data

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,15 +1,16 @@
 import { expect, test } from '@playwright/test';
 import { AuthPage } from '../page-objects/authPage';
+import { IUser } from '../types';
 
-const user = {
+const user: IUser = {
+   name: process.env.USER_NAME,
    email: process.env.EMAIL,
-   password: process.env.PASSWORD,
-   username: process.env.USER_NAME
+   password: process.env.PASSWORD
 }
 
 test('Success login', async ({ page}) => {
    const authPage = new AuthPage(page);
    await authPage.goto();
-   const todayPage = await authPage.login(user.email, user.password);
-   expect(await todayPage.sidebar.getAccountName()).toBe(user.username)
+   const todayPage = await authPage.login(user);
+   expect(await todayPage.sidebar.getAccountName()).toBe(user.name)
 });
