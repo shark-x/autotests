05d2db59caf0a6b761d7b3bff0e89e421be06f6f
shark-x <charming020793@gmail.com>
2023-12-26 00:33:16 +0500

    edited test creation task

--- tests/task.create.test.ts
+++ tests/task.create.test.ts
@@ -1,16 +1,31 @@
-import {expect, test} from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { AuthPage } from '../page-objects/authPage';
 import { TodayPage } from '../page-objects/todayPage';
+import { IUser, ITask } from '../types';
 
-const taskName = 'Test task name';
-const taskDescription = 'Some description';
+const user: IUser = {
+   email: process.env.EMAIL,
+   password: process.env.PASSWORD
+};
+
+const task: ITask = {
+   name: 'Test task name - create',
+   description: 'Some description'
+};
 
 test('Create simple task', async ({ page}) => {
    const authPage = new AuthPage(page);
    await authPage.goto();
-   await authPage.login(process.env.EMAIL, process.env.PASSWORD);
+   await authPage.login(user);
 
    const todayPage = new TodayPage(page);
-   await todayPage.addTask(taskName, taskDescription);
-   expect(await todayPage.isTaskPresent(taskName)).toBeTruthy();
+   const tasksListBefore = await todayPage.getTasksList();
+
+   await todayPage.addTask(task);
+
+   const tasksListAfter = await todayPage.getTasksList();
+   expect(tasksListAfter.total).toBe(tasksListBefore.total+1);
+
+   const lastTask = tasksListAfter.items[tasksListAfter.total-1];
+   expect(lastTask.name).toBe(task.name);
 });
